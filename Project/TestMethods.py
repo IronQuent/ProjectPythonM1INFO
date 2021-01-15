8578a00f8dc7f8a79da17fec079b72c7891e8192
IronQuent <45999146+IronQuent@users.noreply.github.com>
2021-01-15 10:31:18 +0100

    Add unit tests

---
+++ Project/TestMethods.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 15 10:05:59 2021
+
+@author: quentinthomasson
+"""
+
+import unittest
+import Corpus
+import Document
+
+#Classe réalisant les tests unitaires
+class TestMethods(unittest.TestCase):
+    
+    #Vérification du bon fonctionnement de la fonction de nettoyage
+    def testNettoyage(self):
+        testCorpus = Corpus.Corpus("testCorpus")
+        textClean = testCorpus.nettoyer_texte("45Bonjour-321")
+        self.assertEqual(textClean, "  bonjour    ")
+
+    #Verification si le corpus est vide
+    def testCorpusEmpty(self):
+        testCorpus = Corpus.Corpus("testCorpus")
+        self.assertEqual(testCorpus.get_coll(), {})
+    
+    #Verification du type de document 
+    def testDocumentType(self):
+        doc = Document.RedditDocument("2021-01-01", "test", "anonymous", "This is a test doc", "http://test.eu")
+        self.assertEqual(doc.getType(), "Reddit Document")
+    
+#Main
+if __name__ == '__main__':
+    unittest.main()
