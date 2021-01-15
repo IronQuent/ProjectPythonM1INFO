61543802aa610d1e70b1628898cf4b3ddf69ecae
IronQuent <45999146+IronQuent@users.noreply.github.com>
2021-01-15 12:34:26 +0100

    Indent code

--- Project/Author.py
+++ Project/Author.py
@@ -6,23 +6,23 @@ Created on Fri Nov 27 08:12:01 2020
 @author: quentinthomasson
 """
 
-#Classe Auteur
+# Classe Auteur
 class Author():
-    #Constructeur 
-    def __init__(self,name):
+    # Constructeur
+    def __init__(self, name):
         self.name = name
         self.production = {}
         self.ndoc = 0
-        
-    #Ajout d'un document à l'auteur
-    def add(self, doc):     
+
+    # Ajout d'un document à l'auteur
+    def add(self, doc):
         self.production[self.ndoc] = doc
         self.ndoc += 1
 
-    #Affichage 
+    # Affichage
     def __str__(self):
-        return "Auteur: " + self.name + ", Number of docs: "+ str(self.ndoc)
-    
-    #Représentation
+        return "Auteur: " + self.name + ", Number of docs: " + str(self.ndoc)
+
+    # Représentation
     def __repr__(self):
         return self.name
--- Project/Document.py
+++ Project/Document.py
@@ -5,97 +5,97 @@ Created on Fri Nov 27 08:14:10 2020
 
 @author: quentinthomasson
 """
-#Librairie
+# Librairie
 from gensim.summarization.summarizer import summarize
 
-#Classe document
+# Classe document
 class Document():
-    
-    #Constructeur
+
+    # Constructeur
     def __init__(self, date, title, author, text, url):
         self.date = date
         self.title = title
         self.author = author
         self.text = text
         self.url = url
-    
-    #Obtenir l'auteur
+
+    # Obtenir l'auteur
     def get_author(self):
         return self.author
 
-    #Obtenir le titre
+    # Obtenir le titre
     def get_title(self):
         return self.title
-    
-    #Obtenir la date
+
+    # Obtenir la date
     def get_date(self):
         return self.date
-    
-    #Obtenir l'url
+
+    # Obtenir l'url
     def get_source(self):
         return self.source
-    
-    #Obtenir le texte    
+
+    # Obtenir le texte
     def get_text(self):
         return self.text
-    
-    #Affichage
+
+    # Affichage
     def __str__(self):
         return "Document " + str(self.getType()) + " : " + self.title + "\nAuthor : " + self.author
-    
-    #Représentation
+
+    # Représentation
     def __repr__(self):
         return self.title
-    
-    #Résumer
-    def sumup(self,ratio):
+
+    # Résumer
+    def sumup(self, ratio):
         try:
-            auto_sum = summarize(self.text,ratio=ratio,split=True)
+            auto_sum = summarize(self.text, ratio=ratio, split=True)
             out = " ".join(auto_sum)
         except:
-            out =self.title            
+            out = self.title
         return out
-    
-    #Obtenir le type du document
+
+    # Obtenir le type du document
     def getType(self):
         pass
-    
-    
-#Classe Reddit héritant de document   
-class RedditDocument(Document):  
-    #Constructeur
-    def __init__(self, date, title, author, text, url, nbCommentary = 0):
-        super().__init__(date = date, title = title, author = author, text = text, url = url)
+
+
+# Classe Reddit héritant de document
+class RedditDocument(Document):
+    # Constructeur
+    def __init__(self, date, title, author, text, url, nbCommentary=0):
+        super().__init__(date=date, title=title, author=author, text=text, url=url)
         self.nbCommentary = nbCommentary
-        
-    #Obtenir le nombre de commentaires
+
+    # Obtenir le nombre de commentaires
     def get_nbCommentary(self):
         return self.nbCommentary
-    
-    #Affichage
+
+    # Affichage
     def __str__(self):
         return Document.__str__(self) + "\nNb Commentary : " + str(self.nbCommentary)
-    
-    #Obtenir le type
+
+    # Obtenir le type
     def getType(self):
         return "Reddit Document"
 
-#Classe Arxiv héritant de document
+# Classe Arxiv héritant de document
 class ArxivDocument(Document):
-    #Constructeur
-    def __init__(self, date, title, author, text, url, coAuthor = []):
-        super().__init__(date = date, title = title, author = author, text = text, url = url)
+    # Constructeur
+    def __init__(self, date, title, author, text, url, coAuthor=[]):
+        super().__init__(date=date, title=title, author=author, text=text, url=url)
         self.coAuthor = coAuthor
 
-    #Obtenir les co-auteurs
+    # Obtenir les co-auteurs
     def get_coAuthor(self):
         return self.coAuthor
-    
-    #Affichage
+
+    # Affichage
     def __str__(self):
-        #return Document.__str__(self) + "\nCo-Author : " + str(self.coAuthor)
+        # return Document.__str__(self) + "\nCo-Author : " + str(self.coAuthor)
         return Document.__str__(self) + "\nCo-Author : " + ", ".join(self.coAuthor)
-    
-    #Obtenir le type
+
+    # Obtenir le type
     def getType(self):
         return "Arxiv Document"
